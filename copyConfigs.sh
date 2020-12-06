d40bef92d3091775f54de4526a119edc5b3a55a9
Leonardo da Silva Costa <leosilvacosta6@gmail.com>
2020-12-06 16:32:23 -0300

    Inclusao aliases, instalação do doxygen

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -5,3 +5,4 @@ cp -rf $HOME/.config/nvim/* $PWD/neovim/
 cp -f $HOME/.vim/plugged/vim-latex/ftplugin/tex.vim $PWD/vim-latex/
 cp -f $HOME/.vimrc $PWD/
 cp -f $HOME/.config/starship.toml $PWD/
+cp -f $HOME/.bash_aliases $PWD/
