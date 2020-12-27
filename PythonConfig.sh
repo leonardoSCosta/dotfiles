21d34780add436c3c2ed0834b0ed82d9dd1082c0
Leonardo Costa <leosilvacosta6@gmail.com>
2020-12-27 11:01:30 -0300

    Config tensorflow

--- PythonConfig.sh
+++ PythonConfig.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 pip3 install jedi flake8 autopep8 matplotlib testresources
 pip3 install --upgrade tensorflow
+
+#sudo apt install -y libcudart10.1 libcublas10 libcublaslt10 libcufft10 libcurand10 libcusolver10 libcusparse10
+sudo apt install nvidia-cuda-toolkit
