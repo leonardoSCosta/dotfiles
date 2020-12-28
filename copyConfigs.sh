6b222ba1a5d95eff90e4ad1abce44984df51833b
Leonardo Costa <leosilvacosta6@gmail.com>
2020-12-28 10:30:06 -0300

    Função fish

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -7,6 +7,6 @@ cp -rf $HOME/.vim/syntax $PWD/
 cp -rf $HOME/.vimrc $PWD/
 cp -rf $HOME/.config/starship.toml $PWD/
 cp -rf $HOME/.bash_aliases $PWD/
-cp -rf $HOME/Imagens/Papéis\ de\ parede/* $PWD/Wallpapers
-cp -rf $HOME/.config/alacritty $PWD/
+cp -rf $HOME/Imagens/Wallpapers/* $PWD/Wallpapers
+#cp -rf $HOME/.config/alacritty $PWD/
 cp -rf $HOME/.config/fish/config.fish $PWD/fish/
