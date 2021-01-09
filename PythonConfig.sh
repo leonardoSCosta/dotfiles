130cf8ae628c63086137a01c6d6e5f341a3d4e17
Leonardo Costa <leosilvacosta6@gmail.com>
2021-01-09 11:22:50 -0300

    Tensorflow suporte à GPU config

--- PythonConfig.sh
+++ PythonConfig.sh
@@ -2,5 +2,27 @@
 pip3 install jedi flake8 autopep8 matplotlib testresources imutils dlib
 pip3 install --upgrade tensorflow
 
-#sudo apt install -y libcudart10.1 libcublas10 libcublaslt10 libcufft10 libcurand10 libcusolver10 libcusparse10
+# Suporte para GPU no tensorflow
+# Fonte: https://www.tensorflow.org/install/gpu
 sudo apt install nvidia-cuda-toolkit
+
+# Cuda toolkit
+# Fonte: https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=2004&target_type=debnetwork
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
+
+# cuDNN
+# Fonte: https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html#package-manager-ubuntu-install
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-${OS}.pin 
+
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+
+sudo apt-get install -y libcudnn8=8.0.5.39-1+cuda11.1
+sudo apt-get install -y libcudnn8-dev=8.0.5.39-1+cuda11.1
