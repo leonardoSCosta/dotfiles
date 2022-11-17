f46eb840f96ac62b473775af71b87326fa3d6854
Leonardo Costa <leosilvacosta6@gmail.com>
2022-11-17 00:12:55 -0300

    Fix: Colisão entre os robôs e com a borda

--- xscreensaver/robofei/robofei_ssl.c
+++ xscreensaver/robofei/robofei_ssl.c
@@ -20,6 +20,7 @@
 #define OFFSET 100
 #define CENTER_CIRCLE 200
 #define TEAM_SIZE 11
+#define ROBOT_RADIUS 60
 
 Display *display;
 Window window;
@@ -64,28 +65,27 @@ typedef struct
 {
     int x;
     int y;
-    int dx;
-    int dy;
-    int size;
+    float dx;
+    float dy;
+    int radius;
 
 }BALL;
-BALL ball = {.x = OFFSET, .y = OFFSET, .dx = 5, .dy = 5, .size = 21};
+BALL ball = {.x = OFFSET + 50, .y = OFFSET + 50, .dx = 2, .dy = 2, .radius = 21};
 
 typedef struct
 {
     int x;
     int y;
-    int dx;
-    int dy;
-    int size;
+    float dx;
+    float dy;
     int color;
     int id;
 }ROBOT;
 #define BLUE_ROBOT {.x = OFFSET, .y = OFFSET, .dx = 3,\
-                    .dy = 3, .size = 60, .color = BLUE, .id = 0}
+                    .dy = 3, .color = BLUE, .id = 0}
 
 #define YELLOW_ROBOT {.x = OFFSET, .y = OFFSET, .dx = 3,\
-                      .dy = 3, .size = 60, .color = YELLOW, .id = 0}
+                      .dy = 3, .color = YELLOW, .id = 0}
 
 ROBOT teamBlue[TEAM_SIZE] = {
                              BLUE_ROBOT, BLUE_ROBOT, BLUE_ROBOT,
@@ -99,6 +99,41 @@ ROBOT teamBlue[TEAM_SIZE] = {
                                YELLOW_ROBOT,YELLOW_ROBOT,YELLOW_ROBOT,
                                YELLOW_ROBOT, YELLOW_ROBOT
                                };
+
+float distance(const ROBOT *_A, const ROBOT *_B)
+{
+    return sqrt(pow(_A->x - _B->x, 2) + pow(_A->y - _B->y, 2));
+}
+
+float ballDistance(const ROBOT *_A)
+{
+    return sqrt(pow(_A->x - ball.x, 2) + pow(_A->y - ball.y, 2));
+}
+
+void edgeCollision(int *_x, int *_y, float *_dx, float *_dy, int radius)
+{
+    if(*_x - radius/2 <= OFFSET || *_x < 0)
+    {
+        *_x = OFFSET + radius/2;
+        *_dx *= -1;
+    }
+    else if(*_x + radius/2 >= windowAtt.width - OFFSET)
+    {
+        *_x = windowAtt.width - OFFSET - radius/2;
+        *_dx *= -1;
+    }
+
+    if(*_y - radius/2 <= OFFSET || *_y < 0)
+    {
+        *_y = OFFSET + radius/2;
+        *_dy *= -1;
+    }
+    else if(*_y + radius/2 >= windowAtt.height - OFFSET)
+    {
+        *_y = windowAtt.height - OFFSET - radius/2;
+        *_dy *= -1;
+    }
+}
 
 void drawField()
 {
@@ -115,11 +150,13 @@ void drawField()
             OFFSET, windowAtt.height-OFFSET);
     XDrawLine(display, doubleBuffer, graphics, windowAtt.width-OFFSET, OFFSET,
             windowAtt.width-OFFSET, windowAtt.height-OFFSET);
-    XDrawLine(display, doubleBuffer, graphics, windowAtt.width/2-OFFSET, OFFSET,
-            windowAtt.width/2-OFFSET, windowAtt.height-OFFSET);
+    XDrawLine(display, doubleBuffer, graphics, windowAtt.width/2, OFFSET,
+            windowAtt.width/2, windowAtt.height-OFFSET);
 
-    XDrawArc(display, doubleBuffer, graphics, windowAtt.width/2-OFFSET-CENTER_CIRCLE/2,
-            windowAtt.height/2-OFFSET, CENTER_CIRCLE, CENTER_CIRCLE, 0, 360*64);
+    XDrawArc(display, doubleBuffer, graphics,
+             windowAtt.width/2-CENTER_CIRCLE/2,
+             windowAtt.height/2-OFFSET,
+             CENTER_CIRCLE, CENTER_CIRCLE, 0, 360*64);
 
     const int areaHeight = 300, areaWidth = 150;
     XDrawLine(display, doubleBuffer, graphics, OFFSET, windowAtt.height/2 - areaHeight/2,
@@ -146,54 +183,51 @@ void updateBall(BALL *_ball)
     _ball->x += _ball->dx;
     _ball->y += _ball->dy;
 
-    if(_ball->x < OFFSET || _ball->x >= windowAtt.width - OFFSET)
-    {
-        _ball->dx *= -1;
-        return;
-    }
-    if(_ball->y < OFFSET || _ball->y >= windowAtt.height - OFFSET)
-    {
-        _ball->dy *= -1;
-        return;
-    }
+    edgeCollision(&_ball->x, &_ball->y, &_ball->dx, &_ball->dy,
+                  _ball->radius);
 
     unsigned char collision = 0;
+    ROBOT collisionBot;
     for(int i = 0; i < TEAM_SIZE; ++i)
     {
-        if(sqrt(pow(_ball->x - teamBlue[i].x, 2) +
-                pow(_ball->y - teamBlue[i].y, 2)) < _ball->size/2.0 + teamBlue[i].size/2.0)
+        if(ballDistance(&teamBlue[i]) <= _ball->radius/2.0 + ROBOT_RADIUS/2.0)
         {
             collision = 1;
+            collisionBot = teamBlue[i];
             break;
         }
-        if(sqrt(pow(_ball->x - teamYellow[i].x, 2) +
-                pow(_ball->y - teamYellow[i].y, 2)) < _ball->size/2.0 + teamYellow[i].size/2.0)
+        if(ballDistance(&teamYellow[i]) <= _ball->radius/2.0 + ROBOT_RADIUS/2.0)
         {
             collision = 1;
+            collisionBot = teamYellow[i];
             break;
         }
     }
 
     if(collision)
     {
-        _ball->dx /= -_ball->dx;
-        _ball->dy /= -_ball->dy;
-        _ball->dx *= random() % 10 - 5;
-        _ball->dy *= random() % 10 - 5;
+        int speed = random() % 6 + 1;
+        _ball->dx = - speed * (float)(collisionBot.x - _ball->x) / fabs(collisionBot.x - _ball->x + 1e-10);
+        _ball->dy = - speed * (float)(collisionBot.y - _ball->y) / fabs(collisionBot.x - _ball->x + 1e-10);
+    }
 
-        if(_ball->dx == 0 || _ball->dy == 0)
-        {
-            _ball->dx += 1;
-            _ball->dy += 1;
-        }
+    if(fabsf(_ball->dx) < 1 || fabsf(_ball->dx) > 6)
+    {
+        _ball->dx = 1 * _ball->dx/fabsf(_ball->dx);
+    }
+    if(fabsf(_ball->dy) < 1 || fabsf(_ball->dy) > 6)
+    {
+        _ball->dy = 1 * _ball->dy/fabsf(_ball->dy);
     }
 }
 
 void drawBall()
 {
     setColor(ORANGE);
-    XFillArc(display, doubleBuffer, graphics, ball.x, ball.y, ball.size, ball.size,
-            0, 360*64);
+    XFillArc(display, doubleBuffer, graphics,
+             ball.x - ball.radius/2, ball.y - ball.radius/2,
+             ball.radius, ball.radius,
+             0, 360*64);
     updateBall(&ball);
 }
 
@@ -202,86 +236,56 @@ void updateRobot(ROBOT *_robot, int _id)
     _robot->x += _robot->dx;
     _robot->y += _robot->dy;
 
-    if(_robot->x <= OFFSET || _robot->x >= windowAtt.width - OFFSET)
-    {
-        _robot->dx *= -1;
-        return;
-    }
-    if(_robot->y <= OFFSET || _robot->y >= windowAtt.height - OFFSET)
-    {
-        _robot->dy *= -1;
-        return;
-    }
+    edgeCollision(&_robot->x, &_robot->y, &_robot->dx, &_robot->dy,
+                  ROBOT_RADIUS);
 
+    ROBOT collisionBot;
+    unsigned char collision = 0;
     for(int i = 0; i < TEAM_SIZE; ++i)
     {
         if(_robot->color == YELLOW)
         {
-            if(sqrt(pow(teamYellow[_id].x - teamBlue[i].x, 2) +
-                    pow(teamYellow[_id].y - teamBlue[i].y, 2)) <
-                        teamYellow[i].size)
+            if(distance(&teamYellow[_id], &teamBlue[i]) < ROBOT_RADIUS)
             {
-                teamBlue[i].dx = random() % 6 - 3;
-                teamBlue[i].dy = random() % 6 - 3;
-
-                if(teamBlue[i].dx == 0)
-                {
-                    teamBlue[i].dx = 1;
-                }
-                if(teamBlue[i].dy == 0)
-                {
-                    teamBlue[i].dy = 1;
-                }
-
-                teamYellow[_id].dx = -teamBlue[i].dx;
-                teamYellow[_id].dy = -teamBlue[i].dy;
+                collisionBot = teamBlue[i];
+                collision = 1;
             }
-//             if(i < TEAM_SIZE - 1)
-//             {
-//                 if(sqrt(pow(teamYellow[_id].x - teamYellow[i].x, 2) +
-//                             pow(teamYellow[_id].y - teamYellow[i].y, 2)) < teamYellow[_id].size/2.0 + teamBlue[i].size/2.0)
-//                 {
-//                     teamYellow[i].dx *= -1;
-//                     teamYellow[i].dy *= -1;
-//                     teamYellow[_id].dx *= -1;
-//                     teamYellow[_id].dy *= -1;
-//                 }
-//             }
+
+            if(i != _id && distance(&teamYellow[_id], &teamYellow[i]) < ROBOT_RADIUS)
+            {
+                collisionBot = teamYellow[i];
+                collision = 1;
+            }
         }
         else
         {
-            if(sqrt(pow(teamYellow[i].x - teamBlue[_id].x, 2) +
-                    pow(teamYellow[i].y - teamBlue[_id].y, 2)) <
-                        teamYellow[i].size)
+            if(distance(&teamYellow[i], &teamBlue[_id]) < ROBOT_RADIUS)
             {
-                teamYellow[i].dx = random() % 6 - 3;
-                teamYellow[i].dy = random() % 6 - 3;
-
-                if(teamYellow[i].dx == 0)
-                {
-                    teamYellow[i].dx = 1;
-                }
-                if(teamYellow[i].dy == 0)
-                {
-                    teamYellow[i].dy = 1;
-                }
-
-                teamBlue[_id].dx = -teamYellow[i].dx;
-                teamBlue[_id].dy = -teamYellow[i].dy;
+                collisionBot = teamYellow[i];
+                collision = 1;
             }
-
-//             if(i < TEAM_SIZE - 1)
-//             {
-//                 if(sqrt(pow(teamBlue[_id].x - teamBlue[i].x, 2) +
-//                             pow(teamBlue[_id].y - teamBlue[i].y, 2)) < teamBlue[_id].size/2.0 + teamBlue[i].size/2.0)
-//                 {
-//                     teamBlue[i].dx *= -1;
-//                     teamBlue[i].dy *= -1;
-//                     teamBlue[_id].dx *= -1;
-//                     teamBlue[_id].dy *= -1;
-//                 }
-//             }
+            if(i != _id && distance(&teamBlue[_id], &teamBlue[i]) < ROBOT_RADIUS)
+            {
+                collisionBot = teamBlue[i];
+                collision = 1;
+            }
         }
+    }
+
+    if(collision)
+    {
+        int speed = random() % 3 + 1;
+        _robot->dx = - speed * (float)(collisionBot.x - _robot->x) / fabs(collisionBot.x - _robot->x + 1e-10);
+        _robot->dy = - speed * (float)(collisionBot.y - _robot->y) / fabs(collisionBot.x - _robot->x + 1e-10);
+    }
+
+    if(fabsf(_robot->dx) < 1 || fabsf(_robot->dx) > 3)
+    {
+        _robot->dx = 1 * _robot->dx/fabsf(_robot->dx);
+    }
+    if(fabsf(_robot->dy) < 1 || fabsf(_robot->dy) > 3)
+    {
+        _robot->dy = 1 * _robot->dy/fabsf(_robot->dy);
     }
 }
 
@@ -302,10 +306,11 @@ void initRobots()
 void drawRobot(ROBOT *_robot)
 {
     const int blobSize = 15;
-    int size = _robot->size;
+
     setColor(BLACK);
     XFillArc(display, doubleBuffer, graphics,
-            _robot->x - size/2, _robot->y - size/2, size, size,
+            _robot->x - ROBOT_RADIUS/2, _robot->y - ROBOT_RADIUS/2,
+            ROBOT_RADIUS, ROBOT_RADIUS,
             0, 360*64);
     setColor(_robot->color);
     XFillArc(display, doubleBuffer, graphics,
